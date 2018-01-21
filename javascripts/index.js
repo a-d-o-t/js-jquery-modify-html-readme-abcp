ce6004c397de471fcd24ccb94ce5c0846e8237a6
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-21 17:32:19 +0000

    automatically backed up by learn

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,4 +2,4 @@ $(document).ready(function() {
 
  $("#yo").append("hey hey hey hey!!!!!");
  
-})
+})https://learn.co/tracks/accelerated-prep/week-3/week-3-day-2-before-class/js-jquery-modify-html-readme#
